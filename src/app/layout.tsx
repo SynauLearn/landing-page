fd7cfba21aba84e862828bf299a65ad22ff2356d
YohanesVitoMA <yohanes.vito@ti.ukdw.ac.id>
2026-02-06 03:19:12 +0700

    feat: Integrate Vercel Analytics and Speed Insights for performance monitoring.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import ReactLenis from "lenis/react";
 import Header from "@/components/Header";
 import ScrollReset from "@/components/ScrollReset";
 import { APP_NAME } from "@/constants";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: APP_NAME,
@@ -22,6 +24,8 @@ export default function RootLayout({
           <ScrollReset />
           <Header />
           {children}
+          <Analytics />
+          <SpeedInsights />
         </body>
       </ReactLenis>
     </html>
